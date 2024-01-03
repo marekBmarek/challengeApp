39bec5a23b8a57f7d76b30672ee023a6172d47cc
marekBmarek <szymonik1987@gmail.com>
2024-01-03 21:51:23 +0100

    zadanie lekcja 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name10 = "Gabriela";
+bool men = false;
+int age10 = 33;
+if (age10 > 30)
+{
+    if (men == true)
+    {
+        Console.WriteLine("Jestem mezczyzna po trzydziestce");
+    }
+    else if ( name10== "Gabriela")
+    {
+        Console.WriteLine("JESTEM KOBIETA PO TRZYDZIESTCE");
+    }
+}
+else if (age10 < 20) 
+{
+    Console.WriteLine("jetem niepelnoletnim mezczyzna");
+}
