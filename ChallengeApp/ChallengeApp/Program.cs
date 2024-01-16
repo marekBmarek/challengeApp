493a034aa1cba1f7cef2b7349018ab37365e743d
marekBmarek <szymonik1987@gmail.com>
2024-01-16 21:18:24 +0100

    challangeApp

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,19 +1,71 @@
 ﻿
-string name10 = "Gabriela";
-bool men = false;
-int age10 = 33;
-if (age10 > 30)
+int number = 2226675;
+string numberAsstring = number.ToString();
+char[] letters = numberAsstring.ToArray();
+
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+
+foreach (char letter in letters)
 {
-    if (men == true)
+    if (letter == '0')
     {
-        Console.WriteLine("Jestem mezczyzna po trzydziestce");
+        counter0++;
     }
-    else if ( name10== "Gabriela")
+    else if (letter == '1')
     {
-        Console.WriteLine("JESTEM KOBIETA PO TRZYDZIESTCE");
+        counter1++;
+    }
+    if (  letter=='2' ) 
+    {
+        counter2++;
+    }
+    else if(letter == '3') 
+    {
+        counter3++;
+    }
+    if (letter == '4') 
+    {
+        counter4++;    
+    }
+    else if (letter == '5') 
+    {
+        counter5++;
+    }
+    if (letter == '6') 
+    {
+        counter6++;
+    }
+    else if(letter == '7') 
+    {
+        counter7++;    
+    }
+    if (letter == '8') 
+    {
+        counter8++;    
+    }
+    else if(letter == '9') 
+    {
+        counter9++;    
     }
 }
-else if (age10 < 20) 
-{
-    Console.WriteLine("jetem niepelnoletnim mezczyzna");
-}
+Console.WriteLine( "wynik" + number) ;
+Console.WriteLine("0=>" + counter0);
+Console.WriteLine("1=>" + counter1);
+Console.WriteLine("2=>" + counter2);
+Console.WriteLine("3=>" + counter3);
+Console.WriteLine("4=>" + counter4);
+Console.WriteLine("5=>" + counter5);
+Console.WriteLine("6=>" + counter6);
+Console.WriteLine("7=>" + counter7);
+Console.WriteLine("8=>" + counter8);
+Console.WriteLine("9=>" + counter9);
